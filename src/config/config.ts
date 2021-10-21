e79cc402919ca4fff4a991edab12c4353b5c544f
yuregl <yuri.galdino@vendaecia.com>
2021-10-21 20:25:38 -0300

    chore: add enviroment variable salt

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 class Config {
   port: number;
@@ -9,17 +9,18 @@ class Config {
   shopifyKey: string;
   shopifyPassword: string;
   shopifyUrl: string;
+  salt: string;
 
   constructor() {
     dotenv.config();
     this.port = parseInt(process.env.PORT);
     this.mongoUrl = process.env.MONGO_URL;
-    this.dbName = process.env.DB_NAME;
     this.secret = process.env.SECRET;
     this.expiresIn = process.env.EXPIRES_IN;
     this.shopifyKey = process.env.SHOPIFY_KEY_API;
     this.shopifyPassword = process.env.SHOPIFY_PASSWORD;
     this.shopifyUrl = `${process.env.SHOPIFY_URL}`;
+    this.salt = process.env.SALT;
   }
 }
 
