fc8775b37b47405e284f1756eab68126422bae21
yuregl <yuri.galdino@vendaecia.com>
2021-10-21 20:26:41 -0300

    feat: added AuthModule to the imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import Config from './config/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(`${Config.mongoUrl}`), UsersModule],
+  imports: [
+    MongooseModule.forRoot(`${Config.mongoUrl}`),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
