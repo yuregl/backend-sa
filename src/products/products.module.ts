6a1f57973245034e8abbee2f83e437bc86b01f1c
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 10:01:53 -0300

    feat: added HttpModule to imports

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 8000,
+      maxRedirects: 4,
+    }),
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
