0ff3449cf2225ee5877fc652dff008d28c8bb44e
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 10:03:23 -0300

    feat: added product requisition

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { HttpService } from '@nestjs/axios';
+
+import config from '../config/config';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
+  constructor(private httpService: HttpService) {}
 
-  findAll() {
-    return `This action returns all products`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async findAll() {
+    const URL = `https://${config.shopifyKey}:${config.shopifyPassword}${config.shopifyUrl}`;
+    const { data } = await this.httpService.get(URL).toPromise();
+    return data;
   }
 }
