66844a6303ef31647e96cf5fd9eeda8405425961
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 09:15:17 -0300

    feat: added products module

---
+++ src/products/products.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
