018073af83e99ddece0d81968baa2f3cd440b67b
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 11:26:16 -0300

    feat: added authentication in findAll products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.productsService.findAll();
   }
