af6334cea7497f5ca9e212f6ceba723028d09531
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 08:59:13 -0300

    feat: added authService validateUser

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    const compareHash = await bcrypt.compare(pass, user.password);
+    if (user && compareHash) {
+      const { _id, username, email } = user;
+      return { id: _id, username, email };
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
