80e855b56ffab51ef162be8e60f2caf0bdc6ca10
yuregl <yuri.galdino@vendaecia.com>
2021-10-22 10:05:21 -0300

    feat: added httpModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 import Config from './config/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`${Config.mongoUrl}`),
     UsersModule,
     AuthModule,
+    ProductsModule,
+    HttpModule,
   ],
   controllers: [],
   providers: [],
