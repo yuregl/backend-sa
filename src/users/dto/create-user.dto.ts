4fc47f1f97d3593727651d46c86fd224c7410410
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 13:45:12 -0300

    feat: added example swagger user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'queialn@teste.com', description: 'Email user' })
   email: string;
+  @ApiProperty({ example: 'Queilan', description: 'name user' })
   username: string;
+  @ApiProperty({ example: 'senha123', description: 'password user' })
   password: string;
 }
