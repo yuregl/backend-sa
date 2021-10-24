06a53504459e742a34e92d1ebf3265f2046309ab
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 11:25:50 -0300

    feat: added authentication in routes of favorites

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,22 +1,26 @@
-import { Controller, Put, Get, Body, Param } from '@nestjs/common';
+import { Controller, Put, Get, Body, Param, UseGuards } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoriteDto } from './dto/create-favorite.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Put('add')
+  @UseGuards(JwtAuthGuard)
   async createFavorite(@Body() createFavoriteDto: FavoriteDto) {
     return await this.favoritesService.createFavorite(createFavoriteDto);
   }
 
   @Put('remove')
+  @UseGuards(JwtAuthGuard)
   async removeFavorite(@Body() createFavoriteDto: FavoriteDto) {
     return await this.favoritesService.removeFavorite(createFavoriteDto);
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async listUserFavorite(@Param('id') listFavorite: string) {
     return await this.favoritesService.listUserFavorite(listFavorite);
   }
