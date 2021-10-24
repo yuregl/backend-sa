a77e5b246f46b7448244e6836fef51a0728d0b9b
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 11:07:22 -0300

    feat: added to favoriteProducts schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -13,6 +13,9 @@ export class User {
 
   @Prop()
   password: string;
+
+  @Prop({ default: [] })
+  favoritesProducts: [string];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
