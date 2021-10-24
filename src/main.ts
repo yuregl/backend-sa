ba99d1767ea396fd87e33d89c34647834d5ebcb5
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 13:46:10 -0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import config from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Challenge')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
+  console.log(config.port);
+  await app.listen(config.port);
 }
 bootstrap();
