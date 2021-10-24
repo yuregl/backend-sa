4a62fee6f4d34a99d5ff17808b5fa90f6821c648
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 11:09:06 -0300

    feat: added search function for a productId

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,7 +8,13 @@ export class ProductsService {
   constructor(private httpService: HttpService) {}
 
   async findAll() {
-    const URL = `https://${config.shopifyKey}:${config.shopifyPassword}${config.shopifyUrl}`;
+    const URL = `https://${config.shopifyKey}:${config.shopifyPassword}${config.shopifyUrl}.json`;
+    const { data } = await this.httpService.get(URL).toPromise();
+    return data;
+  }
+
+  async getProduct(productId: string) {
+    const URL = `https://${config.shopifyKey}:${config.shopifyPassword}${config.shopifyUrl}/${productId}.json`;
     const { data } = await this.httpService.get(URL).toPromise();
     return data;
   }
