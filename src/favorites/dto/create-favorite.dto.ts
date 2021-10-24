45f025a40423a05d3c939d54985125310f84d325
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 13:46:38 -0300

    feat: added example favorite, swagger

--- src/favorites/dto/create-favorite.dto.ts
+++ src/favorites/dto/create-favorite.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class FavoriteDto {
+  @ApiProperty({
+    example: '6174474fb9d70ede8b94a3fa',
+    description: 'Id User',
+  })
   productId: string;
+
+  @ApiProperty({
+    example: '4543367512203',
+    description: 'Id Product',
+  })
   userId: string;
 }
