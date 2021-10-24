a14531e58b690870decbf74e976d9ac9b2d04e98
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 11:11:59 -0300

    feat: added imports in moduleFavorite

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/entities/user.entity';
+import { HttpModule } from '@nestjs/axios';
+import { ProductsModule } from '../products/products.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    HttpModule.register({
+      timeout: 8000,
+      maxRedirects: 4,
+    }),
+    ProductsModule,
+    UsersModule,
+  ],
   controllers: [FavoritesController],
-  providers: [FavoritesService]
+  providers: [FavoritesService],
 })
 export class FavoritesModule {}
