61a596c3ef0c42a5fa519c9887f727f49621e39b
yuregl <yuri.galdino@vendaecia.com>
2021-10-24 21:21:19 -0300

    chore: adjust enviroment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,10 @@ class Config {
   shopifyPassword: string;
   shopifyUrl: string;
   salt: string;
+  serviceSmtp: string;
+  userSmtp: string;
+  passwordSmtp: string;
+  timeInterval: number;
 
   constructor() {
     dotenv.config();
@@ -21,6 +25,10 @@ class Config {
     this.shopifyPassword = process.env.SHOPIFY_PASSWORD;
     this.shopifyUrl = `${process.env.SHOPIFY_URL}`;
     this.salt = process.env.SALT;
+    this.serviceSmtp = process.env.SERVICE_SMTP;
+    this.userSmtp = process.env.USER_SMTP;
+    this.passwordSmtp = process.env.PASSWORD_SMTP;
+    this.timeInterval = parseInt(process.env.TIME_INTERVAL);
   }
 }
 
